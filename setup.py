eac60b8fab6e93c007f7335cd459b30a86c71cc6
edson <101076486+ed349ti@users.noreply.github.com>
2024-10-19 09:40:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="EdsonGermano",
+    author_email="ed349.ti@gmail.com",
+    description="Meu_Primeiro_Pacote_Python",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ed349ti/simple-package-template"
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.5',
 )
